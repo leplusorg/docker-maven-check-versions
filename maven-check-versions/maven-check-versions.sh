b3179cafd605b4f12b7bc748c6dcd438d969bc5e
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-11-27 19:01:31 -0800

    Fix build

--- maven-check-versions/maven-check-versions.sh
+++ maven-check-versions/maven-check-versions.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC2086
 set -euo pipefail
-set -x
 IFS=$'\n\t'
 
 # shellcheck disable=SC2034
-MAVEN_OPTS=' \
-    -Dhttps.protocols=TLSv1.2 \
-    -Dmaven.repo.local=.m2/repository \
-    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=WARN \
-    -Dorg.slf4j.simpleLogger.showDateTime=true \
-    -Djava.awt.headless=true \
-'
-
-MAVEN_CLI_OPTS=' \
-    --batch-mode \
-    --errors \
-    --fail-at-end \
-    --show-version \
-    -DinstallAtEnd=true \
-    -DdeployAtEnd=true \
-'
+MAVEN_OPTS='-Dhttps.protocols=TLSv1.2 -Dmaven.repo.local=.m2/repository -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=WARN -Djava.awt.headless=true'
+MAVEN_CLI_OPTS='--batch-mode --errors --fail-at-end --show-version -DinstallAtEnd=true -DdeployAtEnd=true'
 
 rc=0
 
