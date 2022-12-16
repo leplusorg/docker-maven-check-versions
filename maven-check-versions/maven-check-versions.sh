457e4d718fb1948dee76d08ddc1fd9bd3e925fd8
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-12-16 12:49:15 -0800

    Update maven-check-versions.sh

--- maven-check-versions/maven-check-versions.sh
+++ maven-check-versions/maven-check-versions.sh
@@ -22,13 +22,6 @@ fi
 
 rc=0
 
-env
-
-\echo "${cmd}" "${opts[@]}" \
-       versions:display-dependency-updates \
-       versions:display-plugin-updates \
-       versions:display-property-updates
-
 "${cmd}" "${opts[@]}" \
        versions:display-dependency-updates \
        versions:display-plugin-updates \
