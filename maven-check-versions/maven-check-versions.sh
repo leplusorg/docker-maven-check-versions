882a56afb95180533e4826e6d52a97ed9e2c8579
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-04-18 18:48:54 -0300

    Update maven-check-versions.sh

--- maven-check-versions/maven-check-versions.sh
+++ maven-check-versions/maven-check-versions.sh
@@ -5,37 +5,37 @@ IFS=$'\n\t'
 shopt -s lastpipe
 
 if [ -f mvnw ]; then
-  cmd='./mvnw'
-  if [ -n "${MAVEN_CONFIG+x}" ]; then
-    # resolve conflict with mvnw
-    unset MAVEN_CONFIG
-  fi
+	cmd='./mvnw'
+	if [ -n "${MAVEN_CONFIG+x}" ]; then
+		# resolve conflict with mvnw
+		unset MAVEN_CONFIG
+	fi
 else
-  cmd='mvn'
+	cmd='mvn'
 fi
 
 if [ -z "${MAVEN_CLI_OPTS+x}" ]; then
-  opts=()
+	opts=()
 else
-  IFS=' ' read -r -a opts <<< "${MAVEN_CLI_OPTS}"
+	IFS=' ' read -r -a opts <<<"${MAVEN_CLI_OPTS}"
 fi
 
 rc=0
 
 "${cmd}" "${opts[@]}" \
-       versions:display-dependency-updates \
-       versions:display-plugin-updates \
-       versions:display-property-updates \
-    | while read -r l; do
-    \echo "${l}"
-    if [[ "${l}" == *"[ERROR]"*
-	|| "${l}" == *"has a newer version:"*
-	|| "${l}" == *"have newer versions:"*
-        || "${l}" == *"update is available:"*
-        || "${l}" == *"updates are available:"*
-        || "${l}" == *"BUILD FAILURE"* ]]; then
-	rc=$((rc+1))
-    fi
-done
+	versions:display-dependency-updates \
+	versions:display-plugin-updates \
+	versions:display-property-updates |
+	while read -r l; do
+		\echo "${l}"
+		if [[ "${l}" == *"[ERROR]"* ||
+			"${l}" == *"has a newer version:"* ||
+			"${l}" == *"have newer versions:"* ||
+			"${l}" == *"update is available:"* ||
+			"${l}" == *"updates are available:"* ||
+			"${l}" == *"BUILD FAILURE"* ]]; then
+			rc=$((rc + 1))
+		fi
+	done
 
 exit ${rc}
