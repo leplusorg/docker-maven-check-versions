0e7fc02050dc586b0f94a25ca9238114e431c394
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-10-28 10:06:51 +0530

    Implement debug mode

--- maven-check-versions/maven-check-versions.sh
+++ maven-check-versions/maven-check-versions.sh
@@ -4,38 +4,58 @@ IFS=$'\n\t'
 
 shopt -s lastpipe
 
+# debug mode is off by default
+if [ -z "${DEBUG+x}" ]; then
+    DEBUG=false
+fi
+
+# Honoring GitHub runner debug mode
+if [ "${ACTIONS_RUNNER_DEBUG}" = true ]; then
+    DEBUG=true
+fi
+
+if [ "${DEBUG}" = true ]; then
+    set -o xtrace
+    \echo "DEBUG: current working directory = $(pwd)"
+    \echo 'DEBUG:'
+    \ls -hal | \sed -e 's/^/DEBUG: /'
+    \echo 'DEBUG:'
+fi
+
 if [ -f mvnw ]; then
-	cmd='./mvnw'
-	if [ -n "${MAVEN_CONFIG+x}" ]; then
-		# resolve conflict with mvnw
-		unset MAVEN_CONFIG
-	fi
+    \echo 'DEBUG: using existing maven wrapper'
+    cmd='./mvnw'
+    if [ -n "${MAVEN_CONFIG+x}" ]; then
+	# resolve conflict with mvnw
+	unset MAVEN_CONFIG
+    fi
 else
-	cmd='mvn'
+    \echo 'DEBUG: using docker-provided maven command'
+    cmd='mvn'
 fi
 
 if [ -z "${MAVEN_CLI_OPTS+x}" ]; then
-	opts=()
+    opts=()
 else
-	IFS=' ' read -r -a opts <<<"${MAVEN_CLI_OPTS}"
+    IFS=' ' read -r -a opts <<<"${MAVEN_CLI_OPTS}"
 fi
 
 rc=0
 
 "${cmd}" "${opts[@]}" \
-	versions:display-dependency-updates \
-	versions:display-plugin-updates \
-	versions:display-property-updates |
-	while read -r l; do
-		\echo "${l}"
-		if [[ "${l}" == *"[ERROR]"* ||
-			"${l}" == *"has a newer version:"* ||
-			"${l}" == *"have newer versions:"* ||
-			"${l}" == *"update is available:"* ||
-			"${l}" == *"updates are available:"* ||
-			"${l}" == *"BUILD FAILURE"* ]]; then
-			rc=$((rc + 1))
-		fi
-	done
+	 versions:display-dependency-updates \
+	 versions:display-plugin-updates \
+	 versions:display-property-updates |
+    while read -r l; do
+	\echo "${l}"
+	if [[ "${l}" == *"[ERROR]"* ||
+		  "${l}" == *"has a newer version:"* ||
+		  "${l}" == *"have newer versions:"* ||
+		  "${l}" == *"update is available:"* ||
+		  "${l}" == *"updates are available:"* ||
+		  "${l}" == *"BUILD FAILURE"* ]]; then
+	    rc=$((rc + 1))
+	fi
+    done
 
 exit ${rc}
