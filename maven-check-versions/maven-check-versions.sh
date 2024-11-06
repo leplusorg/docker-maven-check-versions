2c84c4adbad89df0798b3da2cc292fd0bb01cc4a
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-11-06 21:27:58 +0545

    Do not use exec

--- maven-check-versions/maven-check-versions.sh
+++ maven-check-versions/maven-check-versions.sh
@@ -66,7 +66,7 @@ fi
 
 rc=0
 
-exec "${cmd}" "${opts[@]}" \
+"${cmd}" "${opts[@]}" \
 	versions:display-dependency-updates \
 	versions:display-plugin-updates \
 	versions:display-property-updates |
